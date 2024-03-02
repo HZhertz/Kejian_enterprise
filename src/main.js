79306b6bc894d1ae80e04644cf5842f8f0de1960
XuboZheng <HZhertz76@gmail.com>
2024-03-02 20:37:21 +0800

    init:

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
+//设置超时时间
+axios.defaults.timeout = 5000
+//设置api地址
+//全局定义axios
+axios.defaults.baseURL = 'http://shkjgw.shkjem.com/api/'
+Vue.prototype.$http = axios
+
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +7,102 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/Home.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/news',
+    name: 'news',
+    component: () => import('../views/News.vue')
+  },
+  {
+    path: '/newsdetails/:id',
+    name: 'newsdetails',
+    component: () => import('../views/NewsDetails.vue')
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: () => import('../views/Product.vue')
+  },
+  {
+    path: '/case',
+    name: 'case',
+    component: () => import('../views/Case.vue')
+  },
+  {
+    path: '/casedetails/:id',
+    name: 'casedetails',
+    component: () => import('../views/CaseDetails.vue')
+  },
+  {
+    path: '/goin',
+    name: 'goin',
+    component: () => import('../views/GoIn.vue')
+  },
+  {
+    path: '/download',
+    name: 'download',
+    component: () => import('../views/Download.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    meta: {
+      requireAuth: true
+    },
+    component: () => import('../views/Admin.vue'),
+    children: [
+      {
+        path: '/admin/user',
+        name: 'user',
+        component: () => import('../views/Admin/User.vue')
+      },
+      {
+        path: '/admin/news',
+        name: 'new',
+        component: () => import('../views/Admin/News.vue')
+      },
+      {
+        path: '/admin/cases',
+        name: 'cases',
+        component: () => import('../views/Admin/Cases.vue')
+      },
+      {
+        path: '/admin/team',
+        name: 'team',
+        component: () => import('../views/Admin/Team.vue')
+      },
+      {
+        path: '/admin/course',
+        name: 'course',
+        component: () => import('../views/Admin/Course.vue')
+      },
+      {
+        path: '/admin/enterprise',
+        name: 'enterprise',
+        component: () => import('../views/Admin/Enterprise.vue')
+      },
+      {
+        path: '/admin/honor',
+        name: 'honor',
+        component: () => import('../views/Admin/Honor.vue')
+      },
+      {
+        path: '/admin/dictionary',
+        name: 'dictionary',
+        component: () => import('../views/Admin/Dictionary.vue')
+      },
+      {
+        path: '/admin/page',
+        name: 'page',
+        component: () => import('../views/Admin/Page.vue')
+      }
+    ]
   }
 ]
 
