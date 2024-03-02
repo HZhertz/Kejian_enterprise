4a183c50ae506454d8ea32f39ba903158bf79d12
XuboZheng <HZhertz76@gmail.com>
2024-03-02 22:01:28 +0800

    feat(Home): 新增首页

--- src/main.js
+++ src/main.js
@@ -4,8 +4,22 @@ import router from './router'
 import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import VueLazyload from 'vue-lazyload'
 
 Vue.use(ElementUI)
+Vue.use(VueAwesomeSwiper)
+Vue.use(VueLazyload, {
+  //完全显示的时候加载
+  preLoad: 1,
+  //失败时显示的图片
+  error: require('./assets/img/error.png'),
+  //加载时显示的GIF图
+  loading: require('./assets/img/loading.gif'),
+  //尝试加载几次
+  attempt: 1
+})
 
 Vue.config.productionTip = false
 
@@ -15,6 +29,8 @@ axios.defaults.timeout = 5000
 //全局定义axios
 axios.defaults.baseURL = 'http://shkjgw.shkjem.com/api/'
 Vue.prototype.$http = axios
+// 图片服务器地址
+Vue.prototype.imgserver = 'http://shkjgw.shkjem.com/'
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,322 @@
 <template>
-  <div>Home</div>
+  <div class="home" v-loading="loading">
+    <swiper id="swiperBox" v-bind:options="swiperOption" ref="mySwiper">
+      <swiper-slide class="swiper-slide slide-one">
+        <div class="page">
+          <h3>科建股份</h3>
+          <p>KeJian Stock</p>
+        </div>
+        <p class="slogan">立人立己 达人达己</p>
+      </swiper-slide>
+      <swiper-slide class="swiper-slide slide-two">
+        <div class="page">
+          <h3>经典案例</h3>
+          <p>Suecessful Cass</p>
+        </div>
+        <ul class="case-item">
+          <li
+            v-for="(item, index) in caseList"
+            :key="index"
+            v-lazy:background-image="imgserver + item.Img"
+          >
+            <router-link
+              class="text-decoration"
+              :to="{ name: 'casedetails', params: { id: item.Id } }"
+            >
+              <div class="case-item-hover">
+                <p class="hover-title">{{ item.Title }}</p>
+                <div class="bottom"></div>
+                <div class="more">
+                  <span>MORE</span>
+                </div>
+              </div>
+            </router-link>
+          </li>
+        </ul>
+      </swiper-slide>
+      <swiper-slide class="swiper-slide slide-three">
+        <div class="page">
+          <h3>最新资讯</h3>
+          <p>Latest News</p>
+        </div>
+        <div class="news-content">
+          <div class="news-content-item" v-for="(news, i) in newsList" :key="i">
+            <div :style="'order: ' + (i % 2 == 0 ? 1 : 3)">
+              <router-link
+                class="text-decoration"
+                :to="{ name: 'newsdetails', params: { id: news.Id } }"
+              >
+                <div class="item-img" v-lazy:background-image="imgserver + news.Img"></div>
+              </router-link>
+            </div>
+            <div style="order: 2">
+              <el-divider>
+                <i class="el-icon-apple"></i>
+              </el-divider>
+            </div>
+            <div class="item-content" :style="'order: ' + (i % 2 == 0 ? 3 : 1)">
+              <h3>{{ news.Title }}</h3>
+              <p>{{ news.Content }}</p>
+              <span>{{ news.CreateTime }}</span>
+            </div>
+          </div>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script>
-export default {}
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  name: 'HelloWorld',
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+      loading: true,
+      caseList: [],
+      newsList: [],
+      swiperOption: {
+        notNextTick: true, //notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
+        direction: 'vertical',
+        grabCursor: true,
+        setWrapperSize: true,
+        autoHeight: true,
+        slidesPerView: 1,
+        mousewheel: true,
+        mousewheelControl: true, //同上
+        height: window.innerHeight - 60,
+        resistanceRatio: 0,
+        observeParents: true,
+        // https://www.swiper.com.cn/api/index.html SwiperApi
+
+        // 如果自行设计了插件，那么插件的一些配置相关参数，也应该出现在这个对象中，如下debugger
+        //debugger: true,
+
+        // swiper的各种回调函数也可以出现在这个对象中，和swiper官方一样
+        on: {
+          //监听滑动切换事件，返回swiper对象
+          // slideChange: () => {
+          //   let swiper = this.$refs.mySwiper.swiper;
+          //   //console.log(swiper.activeIndex); //滑动打印当前索引
+          //   if (swiper.activeIndex === this.list.length - 1) {
+          //     //到最后一个加载更多数据
+          //     let newList = [];
+          //     let listLength = this.list.length;
+          //     for (let i = 0; i < 10; i++) {
+          //       newList.push(listLength + i);
+          //     }
+          //     this.list = this.list.concat(newList);
+          //   }
+          // }
+        }
+      }
+    }
+  },
+  created() {},
+  // 如果你需要得到当前的swiper对象来做一些事情，你可以像下面这样定义一个方法属性来获取当前的swiper对象，同时notNextTick必须为true
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper
+    }
+  },
+  mounted() {
+    Promise.all([this.$http.get('Cases/GetCasesAll'), this.$http.get(`News?type=1&num=3`)]).then(
+      ([responseCases, responseNews]) => {
+        this.caseList = responseCases.data
+        this.newsList = responseNews.data
+        this.loading = false
+      }
+    )
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.swiper-slide {
+  font-size: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+
+  .page {
+    text-align: center;
+    height: 100px;
+    overflow: hidden;
+    h3,
+    p {
+      font-size: 40px;
+      font-weight: 400;
+      color: #fff;
+    }
+  }
+
+  .slogan {
+    text-align: center;
+    font-size: 50px;
+    color: #fff;
+    padding: 30px 0;
+  }
+}
+//经典案例
+.case-item {
+  width: 1100px;
+  height: 500px;
+  overflow: hidden;
+  margin: 0 auto;
+  margin-top: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  li {
+    width: 330px;
+    height: 250px;
+    list-style: none;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-origin: content-box;
+    margin: 5px;
+    position: relative;
+    overflow: hidden;
+
+    &:hover {
+      .case-item-hover {
+        opacity: 1;
+        transition: all 0.4s ease-in-out;
+      }
+    }
+  }
+}
+//经典案例hover
+.case-item-hover {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  overflow: hidden;
+  background-color: rgba(225, 56, 52, 0.7);
+
+  .hover-title {
+    height: 50px;
+    color: #fff;
+    font-size: 18px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-weight: 400;
+    margin-top: 20px;
+  }
+  .bottom {
+    border-bottom: 1px solid #fff;
+    width: 60px;
+    margin: 0 auto;
+  }
+  .more {
+    width: 90px;
+    padding: 5px 5px;
+    margin: 0 auto;
+    margin-top: 100px;
+    border: 2px solid #fff;
+    span {
+      color: #fff;
+      font-size: 20px;
+    }
+  }
+}
+
+.slide-one {
+  background: url(../assets/img/home_top.jpg) no-repeat center;
+  background-size: cover;
+}
+.slide-two {
+  background: url(../assets/img/home_do.jpg) no-repeat center;
+  background-size: cover;
+}
+.slide-three {
+  background: url(../assets/img/home_anli.jpg) no-repeat center;
+  background-size: cover;
+}
+//最新资讯
+.news-content {
+  width: 1240px;
+  margin: 0 auto;
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+
+  &-item {
+    width: 400px;
+    display: flex;
+    flex-direction: column;
+
+    .item-img {
+      width: 360px;
+      height: 230px;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+      background-origin: content-box;
+      margin: 0 auto;
+    }
+    .el-divider {
+      background-color: #fff;
+      height: 3px;
+      .el-divider__text {
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        padding: 0px;
+        color: #fff;
+      }
+    }
+    .item-content {
+      width: 360px;
+      height: 230px;
+      margin: 0 auto;
+      //border: 1px solid paleturquoise;
+      h3 {
+        font-size: 22px;
+        height: 30px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      p {
+        font-size: 15px;
+        height: 80px;
+        overflow: hidden;
+        margin: 10px 0;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 4;
+        -webkit-box-orient: vertical;
+        white-space: normal !important;
+        word-wrap: break-word;
+      }
+      span {
+        display: block;
+        font-size: 14px;
+        text-align: end;
+      }
+      h3,
+      p,
+      span {
+        color: #fff;
+      }
+    }
+  }
+}
+.order {
+  order: -1;
+}
+.order-img {
+  order: 1;
+}
+</style>
